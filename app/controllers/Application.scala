b625c34bde5cbccae5f177bfb6e64e8a9c5cf397
Athiwat Chunlakhan <athiwatc@gmail.com,>
2012-08-08 05:45:48 +0700

    Add server logic for setting the access key, so we can logout

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,21 @@ import play.api.mvc._
 
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index = Action { request =>
+   request.session.get("token").map { token =>
+   Ok(views.html.index(token))
+  }.getOrElse {
+    Ok(views.html.index(""))
+  }
+    
+  }
+  
+  def settoken(token: String) = Action { 
+    Redirect("/").withSession({"token" -> token})
+  }
+  
+  def logout() = Action {
+    Redirect("/").withNewSession
   }
   
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,16 @@
-var ClientID = 'de6f95b7322f49c7b8156fbf8e8a5292'
-if(location.href.match(":9000")) {
-	ClientID = '87ce87f5fd3645cca784116b3ad28860';
-}
-
 function getUrlVar(key){
 	var result = new RegExp(key + "=([^&]*)", "i").exec(window.location.hash); 
 	return result && unescape(result[1]) || ""; 
 }
 var AccessToken = getUrlVar('access_token');
-var nextURL = "https://api.instagram.com/v1/users/self/feed/?COUNT=15&access_token="+AccessToken;
-
+var nextURL = "https://api.instagram.com/v1/users/self/feed/?COUNT=15&access_token="+ServerToken;
+var isLoading = false;
 
 var container = $$({}, '<div class="container">');
 $$.document.append(container);
 
-if (AccessToken != "") {
+if (ServerToken != "") {
+
 	$.ajax({
         type: "GET",
         dataType: "jsonp",
@@ -27,23 +23,34 @@ if (AccessToken != "") {
 			container.append(temp);
 		}
 	}});
+	
+	
+	$.ajax({
+        type: "GET",
+        dataType: "jsonp",
+        cache: false,
+        url:  "https://api.instagram.com/v1/users/self/?&access_token="+ServerToken	,
+        success:  function(data) {
+		$("#username").text(data.data.full_name);
+        if (data.data.full_name == "") $("#username").text(data.data.username);
+		$("#logoutBox").attr("hidden", false);
+	}});
 
-
+} else if (AccessToken != "") {
+	
+	window.location = "/settoken/" + AccessToken;
 
 } else {
 
-var loginButton = $$({text: "Login"}, '<div id="loginBtn" class="btn btn-primary" data-bind="text">',
-{
-	'click &': function(){
-		window.location.href = "https://instagram.com/oauth/authorize/?display=touch&client_id="+ClientID+"&redirect_uri="+location.href+"&response_type=token";
-	}
-});
-container.append(loginButton);
+	$("#loginBox").attr("hidden", false);
+	$("#loginBtn").attr("href", "https://instagram.com/oauth/authorize/?display=touch&client_id="+ClientID+"&redirect_uri="+location.href+"&response_type=token");
 
 }
 
 $(window).scroll(function() {
-   if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+   if($(window).scrollTop() + $(window).height() > $(document).height() - 500) {
+   if (isLoading == false) {
+  		 isLoading = true;
        $.ajax({
         type: "GET",
         dataType: "jsonp",
@@ -55,7 +62,9 @@ $(window).scroll(function() {
 			var temp = $$({}, "<img src='" + data.data[i].images.low_resolution.url +"'>");
 			container.append(temp);
 		}
+		isLoading = false;
 	}});
+	}
    }
 });
 

e4b01a21d00229ceb8ee9c7bd8391973b11b36c9
Athiwat Chunlakhan <athiwatc@gmail.com,>
2012-08-08 06:03:55 +0700

    Change README File and remove about and contact
    also remove the link in home

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,22 +4,22 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
-  
-  def index = Action { request =>
-   request.session.get("token").map { token =>
-   Ok(views.html.index(token))
-  }.getOrElse {
-    Ok(views.html.index(""))
-  }
-    
-  }
-  
-  def settoken(token: String) = Action { 
-    Redirect("/").withSession({"token" -> token})
-  }
-  
-  def logout() = Action {
-    Redirect("/").withNewSession
-  }
-  
+
+	def index = Action { request =>
+	request.session.get("token").map { token =>
+	Ok(views.html.index(token))
+	}.getOrElse {
+		Ok(views.html.index(""))
+	}
+
+	}
+
+	def settoken(token: String) = Action { 
+		Redirect("/").withSession({"token" -> token})
+	}
+
+	def logout() = Action {
+		Redirect("/").withNewSession
+	}
+
 }

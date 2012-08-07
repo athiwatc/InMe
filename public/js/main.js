eb830dfaf0fe2e07f4b4cce63f9ddc78cc118bb4
Athiwat Chunlakhan <athiwatc@gmail.com,>
2012-08-08 03:10:53 +0700

    Add oauth

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,20 @@
-var ClientID = de6f95b7322f49c7b8156fbf8e8a5292
 
-if (location.href.match(/:9000)) {
+
+var ClientID = 'de6f95b7322f49c7b8156fbf8e8a5292'
+
+if(location.href.match(":9000")) {
 	ClientID = '87ce87f5fd3645cca784116b3ad28860';
 }
+
+
+
+var container = $$({}, '<div class="container">');
+var loginButton = $$({text: "Login"}, '<div id="loginBtn" class="btn btn-primary" data-bind="text">',
+{
+	'click &': function(){
+		window.location.href = "https://instagram.com/oauth/authorize/?display=touch&client_id="+ClientID+"&redirect_uri="+location.href+"oauth&response_type=token";
+	}
+});
+container.append(loginButton);
+
+$$.document.append(container);
